8e3dcf38df6a32cf86b0d368cd0600571a0182ee
julyvjo <julyvjo@gmail.com>
2021-05-20 19:55:38 -0300

    socket llamados ahora soporta retry a server de reserva

--- Llamado_de_clientes/src/negocio/Socket_Llamado.java
+++ Llamado_de_clientes/src/negocio/Socket_Llamado.java
@@ -1,33 +1,88 @@
 package negocio;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 import dominio.Box;
 import dominio.Turno;
 
 public class Socket_Llamado {
+	private int port1 = 5000;
+	private int port2 = 5100;
+	private int port = port1;
 
 	public Turno getTurno(Box box) {
 		Turno turno = null;
+		
 		try {
-			Socket socket = new Socket("localhost",5555);
 			
-			ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
-			output.writeObject(box);
+			turno = conecta(port, box);
 			
-			ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
-			turno = (Turno) input.readObject();
-			
-			output.close();
-			input.close();
-			socket.close();
-			
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+		} catch (UnknownHostException e) { //falla al conectar con el port
+				
+				if(port == port1) { //si no puede conectar con el primer puerto se cambia el port principal por el de reserva (sea cual sea de los dos)
+					port = port2;
+				}else {
+					port = port1;
+				}
+				
+				try {
+					
+					turno = conecta(port, box); //reintenta conectar usando el port de reserva
+					
+				} catch (UnknownHostException e1) {
+					e1.printStackTrace();
+				} catch (ClassNotFoundException e1) {
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+		}catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		return turno;
 	}
+	
+	public Turno conecta(int port, Box box) throws UnknownHostException, IOException, ClassNotFoundException {
+		Turno turno = null;
+		
+		Socket socket = new Socket("localhost",port);
+		
+		ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+		output.writeObject(box);
+		
+		ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+		turno = (Turno) input.readObject();
+		
+		output.close();
+		input.close();
+		socket.close();
+		
+		return turno;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
