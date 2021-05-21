7e583308a491f8208d4152f6b4b3a1bbe21a304b
Martin Vila <vilamartin21@gmail.com>
2021-05-21 18:39:26 -0300

    Update Socket_Registro.java

--- Registro_de_clientes/src/negocio/Socket_Registro.java
+++ Registro_de_clientes/src/negocio/Socket_Registro.java
@@ -1,27 +1,53 @@
 package negocio;
 
+import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 import dominio.Cliente;
 
 public class Socket_Registro {
+	private final int port1 = 4000;
+	private final int port2 = 4100;
+	private int port = port1;
+	
 	
 	public void agregarCliente(Cliente cliente) {
 		try {
-			Socket socket = new Socket("localhost",4444);
+			conecta(port, cliente);
 			
-			ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
-			
-			output.writeObject(cliente);
-			
-			
-			output.close();
-			socket.close();
-			
-			} catch (Exception e) {
-				e.printStackTrace();
+		} catch(UnknownHostException e) { 		//falla al conectar con el port
+			if(port == port1) 					//si no puede conectar con el primer puerto se cambia el port principal por el de reserva (sea cual sea de los dos)
+				port = port2;
+			else
+				port = port1;
+				
+			try {		
+				conecta(port, cliente); 		//reintenta conectar usando el port de reserva
+				
+			} catch(UnknownHostException e1) {
+				e1.printStackTrace();
+				
+			} catch(IOException e1) {
+				e1.printStackTrace();
 			}
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public void conecta(int port, Cliente cliente) throws UnknownHostException, IOException {
+		Socket socket = new Socket("localhost", port);
+			
+		ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+			
+		output.writeObject(cliente);
+			
+		output.close();
+		socket.close();
 	}
 	
 }
